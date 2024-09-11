954a3a62c50d489103eba0d413013c2377442b0c
vanessaferrer <vanessalimaoi729@gmail.com>
2024-09-11 11:20:49 +0000

    Pending changes exported from your codespace

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
   console.log("Hello Rigo from the console!");
+  let whoRandom = who[Math.floor(Math.random() * who.length)];
+  let actionRandom = action[Math.floor(Math.random() * action.length)];
+  let whatRandom = what[Math.floor(Math.random() * what.length)];
+  let whenRandom = when[Math.floor(Math.random() * when.length)];
+
+  let excuse =
+    whoRandom + " " + actionRandom + " " + whatRandom + " " + whenRandom;
+  const myTitle = document.getElementById("excuse");
+  myTitle.innerHTML = excuse;
 };
